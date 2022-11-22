2856af24cc8589a35312da918660ebd1a89b71c3
unknown <dzh.3743@gmail.com>
2022-11-22 11:53:50 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
 
 const DEBOUNCE_DELAY = 300;
